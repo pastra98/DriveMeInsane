cbcdddce683aacb7b781286e61c7f46adf80b06c
Paul Straberger <p.straberger@gmail.com>
2022-11-06 01:17:29 +0100

    prepare for connecting to gui

--- Player/player.gd
+++ Player/player.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
-# Called when the node enters the scene tree for the first time.
+signal passenger_added(name, imgpath, insanity)
+
+
 func _ready():
     add_passenger("Bob")
+    connect("passenger_added", GuiManager, "add_passenger_window")
 
 
 func _physics_process(delta): # testing
@@ -16,3 +19,10 @@ func get_current_kph() -> float:
 func add_passenger(passenger_name: String):
     var new_passenger = Passenger.new(passenger_name)
     $Car/PassengerManager.add_child(new_passenger)
+    emit_signal(
+        "passenger_added",
+        "res://Player/Passengers/%s.png" % passenger_name,
+        new_passenger
+        )
+
+# andere idee: ich schicke hier eine reference zu passenger mit. diese wird im gui manager nur verwendet um eine connection vom passenger zum guiPassengerWindow zu machen. Die connection wird also durchs gui gemacht. danach wird die reference nie wieder verwendet.
