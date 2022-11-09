6440a3299d9196caf72340a6ad0b0ce8dafd6bdb
Paul Straberger <p.straberger@gmail.com>
2022-11-09 10:43:40 +0100

    start adding level picker

--- GUI/gui_manager.gd
+++ GUI/gui_manager.gd
@@ -1,10 +1,10 @@
 extends CanvasLayer
 
-onready var player = $"/root/Level/Player"
-onready var player_status = $"PlayerStatus"
+var player
+var player_status
 
 func _ready():	
-    player.connect("new_player_health", player_status, "update_health")
+    pass
 
 
 func add_passenger_window(passenger_ref):
@@ -14,3 +14,13 @@ func add_passenger_window(passenger_ref):
     $PassengerMargin/PassengerContainer.add_child(passenger_window)
     passenger_window.update_insanity(passenger_ref.insanity, "Lets start!")
     passenger_window.update_picture(passenger_ref.imgpath % "happy")
+
+
+func add_player_status():
+    player_status = load("res://gui/player_status/PlayerStatus.tscn").instance()
+    add_child(player_status)
+    player.connect("new_player_health", player_status, "update_health")
+
+
+func show_level_picker():
+    var lvl_picker = load("res://gui/level_picker/level_picker.gd").instance()
---
+++ GUI/level_picker/level_picker.gd
@@ -0,0 +1,13 @@
+extends MarginContainer
+
+"""
+TODO:
+- make this whole thing more procedural
+"""
+
+signal new_level_picked(lvl_nr)
+
+func _ready():
+    # $Panel/VBoxContainer/Lvl1.connect()
+    pass
+
