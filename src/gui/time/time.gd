fe31045f9e60e33fa3baf11bd4837a7000307f6f
Paul Straberger <p.straberger@gmail.com>
2022-11-14 23:07:20 +0100

    very simple timer

---
+++ src/gui/time/time.gd
@@ -0,0 +1,8 @@
+extends Label
+
+onready var level_timer = $"/root/Main/Level/Timer"
+
+func _process(delta):
+    text = "%d:%02d" % [floor(level_timer.time_left / 60), int(level_timer.time_left) % 60]
+    # TODO: make text red once under 10 seconds
+    # TODO: this can actually be a script without a whole scene attached to it
