3561d322d3984c3f9876928499aef2fbff69d58f
Paul Straberger <p.straberger@gmail.com>
2022-11-14 23:05:14 +0100

    start working on level timer and win/lose conditions etc

--- src/environment/level.gd
+++ src/environment/level.gd
@@ -6,8 +6,10 @@ export(int) var points_3_stars = null
 export(int) var time_to_complete = null
 
 var player
-var is_playing = false
+var point_levels = [points_1_star, points_2_stars, points_3_stars]
 
+var timer: Timer
+var available_passengers
 
 func _ready():
     # do some checks here if the exports have been set by the scene
@@ -15,9 +17,14 @@ func _ready():
         if property.usage == 8199 and get(property.name) == null:
             print("Property %s\nHas not been set in the inspector" % property.name); breakpoint
     # add container for passengers during level picking
-    var available_passengers = Node2D.new()
+    available_passengers = Node2D.new()
     available_passengers.name = "AvailablePassengers"
     add_child(available_passengers)
+    # add timer node
+    timer = Timer.new() # TODO: maybe later ticking sound when timer is low
+    timer.name = "Timer"
+    timer.connect("timeout", self, "time_up")
+    add_child(timer)
 
 # ---------- BEFORE GAME STARTS ----------
 
@@ -30,30 +37,49 @@ func prepare_level(unlocked_passengers: Array):
     for passenger_name in unlocked_passengers:
         var new_passenger = Passenger.new(passenger_name)
         passenger_instances.append(new_passenger)
-        $"AvailablePassengers".add_child(new_passenger)
+        available_passengers.add_child(new_passenger)
     GuiManager.add_passenger_picker(passenger_instances)
 
 
 func add_passenger_to_player(passenger_ref: Passenger):
-    $"AvailablePassengers".remove_child(passenger_ref)
+    available_passengers.remove_child(passenger_ref)
     player.passengers.add_child(passenger_ref)
 
 
 func remove_passenger_from_player(passenger_ref: Passenger, _seat_name: String):
     # TODO: fix this- super hacky, seat name is not needed here but since we only use one signal for gui and level, we do it this way
     player.passengers.remove_child(passenger_ref)
-    $"AvailablePassengers".add_child(passenger_ref)
+    available_passengers.add_child(passenger_ref)
 
 # ----------  GAME STARTS ----------
 
 func start_level(): # probably is going to be triggered by button in picker
     # free unused passengers that are still children of AvailablePassengers
-    for passenger in $"AvailablePassengers".get_children():
+    for passenger in available_passengers.get_children():
         passenger.queue_free()
+    # connect selected passengers to player
+    var passenger_refs = player.get_node("Car/PassengerManager").get_children()
+    for passenger in passenger_refs:
+        passenger.connect("passenger_raging", player, "_on_raging_passenger")
     # set up the gui
-    GuiManager.add_player_status(player)
-    for passenger in player.get_node("Car/PassengerManager").get_children():
-        GuiManager.add_passenger_window(passenger)
-    # TODO: maybe not do this so directly
-    GuiManager.get_node("PassengerPicker").queue_free()
-    is_playing = true
+    GuiManager.load_game_hud(player, passenger_refs)
+    # delay a bit and then start timer
+    yield(get_tree().create_timer(1), "timeout") # TODO: clean wait until level starts - also disable driving car
+    timer.start(time_to_complete)
+
+
+func player_crashed():
+    level_passed()
+
+
+func time_up():
+    GuiManager.clear_game_hud()
+    level_passed()
+
+
+func level_passed():
+    pass
+
+
+func level_failed():
+    pass
