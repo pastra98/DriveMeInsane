befc7dcc72afbcdf5cc64a023f3bb37a81573147
Paul Straberger <p.straberger@gmail.com>
2022-11-14 23:07:04 +0100

    forgot we can already track score in PlayerSTatus

--- src/gui/player_status/player_status.gd
+++ src/gui/player_status/player_status.gd
@@ -2,7 +2,13 @@ extends MarginContainer
 
 
 onready var bar = $HBoxContainer/Health
+onready var score = $HBoxContainer/Score
 
 
-func update_health(new_value):
-    bar.value = new_value
+func update_health(new_health):
+    bar.value = new_health
+
+
+func update_score(new_score):
+    # TODO: later show how many pts missing for pass level /next star 
+    score.text = str(new_score)
