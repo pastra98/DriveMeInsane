264ab52bd5402801c60b99391215f4d64170432a
Raphael Pruckner <rapppruck@gmail.com>
2022-11-21 16:01:16 +0100

    feat: implement dynamic smoke scaling

--- src/player/smoke.gd
+++ src/player/smoke.gd
@@ -1,16 +1,34 @@
 extends CPUParticles2D
 
+onready var player = get_node("/root/Main/Level/PlayerPos/Player")
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# yes, this is very verbose and could have been done using a single array,
+# but for readability purposes it's like this
 
+# Amount scaling was initially tried, however Godot resets all particles after
+# changing particle amount
 
-# Called when the node enters the scene tree for the first time.
+var initial_lifetime = lifetime
+var initial_scale = scale_amount 
+var initial_spread = spread
+
+const max_lifetime = 0.8
+const max_scale = 40
+const max_spread = 20
+
+const top_speed = 50
+
 func _ready():
     pass 
 
-
-
 func _process(delta):
-   pass
+    set_lifetime(calculate_smoke(initial_lifetime, max_lifetime))
+    set_param(PARAM_SCALE, calculate_smoke(initial_scale, max_scale))
+    set_spread(calculate_smoke(initial_spread, max_spread))
+
+
+func calculate_smoke(initial, maximum):
+    var scale_speed = player.get_current_kph() / float(top_speed)
+    var factor = (maximum / float(initial) - 1) * scale_speed + 1
+    return initial * factor
+    
