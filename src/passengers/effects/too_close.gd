ff217548c4b96424f3773c8cf1e4961ec6c11de3
Paul Straberger <p.straberger@gmail.com>
2022-11-26 21:34:56 +0100

    not sure if this is better

--- src/passengers/effects/too_close.gd
+++ src/passengers/effects/too_close.gd
@@ -3,6 +3,7 @@ extends Area2D
 
 signal change_insanity(amt, reason, is_broadcast)
 onready var player = $"/root/Main/Level".player
+onready var tilemap = $"/root/Main/Level/layer1"
 
 var too_fast: int
 var coll_size_mult: float
@@ -10,15 +11,14 @@ var coll_size_mult: float
 var collider: CollisionShape2D
 
 var insanity_effect: float
-var cooldown: int
-var is_on_cooldown = false
+
+var t = 0
 
 func _init(speed_tol: int, coll_s_mult: float, insanity_eff: float, cooldown_time: int):
     too_fast = speed_tol
     coll_size_mult = coll_s_mult
     insanity_effect = insanity_eff
     connect("body_entered", self, "_on_body_entered")
-    cooldown = cooldown_time
     name = "TooClose"
 
 func _ready():
@@ -31,17 +31,19 @@ func _ready():
     collider.name = "TooCloseCollider"
     add_child(collider)
 
-func _on_body_entered(body:Node):
-    if body.is_in_group("static_env") and player.get_current_kph() > too_fast and not is_on_cooldown:
+func _physics_process(delta):
+    print(get_overlapping_bodies())
+    t += delta
+    if t > 0.2 and player.get_current_kph() > too_fast and overlaps_body(tilemap):
         emit_signal("change_insanity", insanity_effect, "Too close!!", false)
-        get_node(name + "Timer").start(cooldown)
-        is_on_cooldown = true
+        t = 0
+# func _on_body_entered(body:Node):
+#     if body.is_in_group("static_env") and player.get_current_kph() > too_fast and not is_on_cooldown:
+#         emit_signal("change_insanity", insanity_effect, "Too close!!", false)
+#         get_node(name + "Timer").start(cooldown)
+#         is_on_cooldown = true
 
 
 func get_txt_description() -> String:
-    return "%s:\n+%s insanity when close to other objects" % [name, insanity_effect]
-    # TODO: need better descriptions
-
-
-func cooldown_over():
-    is_on_cooldown = false
+    # return "%s:\n+%s insanity when close to other objects" % [name, insanity_effect]
+    return ""
