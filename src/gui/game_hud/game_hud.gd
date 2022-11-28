173346168464afe1f5a3ac0b7cf097c8bf333359
Paul Straberger <p.straberger@gmail.com>
2022-11-28 11:14:15 +0100

    countdown in timer implemented

--- src/gui/game_hud/game_hud.gd
+++ src/gui/game_hud/game_hud.gd
@@ -1,10 +1,16 @@
 extends Control
 
 onready var level_timer = $"/root/Main/Level/Timer"
+var beepsound = load("res://audio/sounds/beep.wav")
+var time_fmt = "[center]%d:%02d"
+var prev_t: int = 100000 # don't question it
 
 func _process(delta):
-    # TODO: write this intelligently
-    if level_timer.time_left < 10:
-        $"TimeLabel".bbcode_text = "[color=red][center]%d:%02d" % [floor(level_timer.time_left / 60), int(ceil(level_timer.time_left)) % 60 % 60]
-    else:
-        $"TimeLabel".bbcode_text = "[center]%d:%02d" % [floor(level_timer.time_left / 60), int(ceil(level_timer.time_left)) % 60 % 60]
+    var cur_t: int = ceil(level_timer.time_left)
+    if cur_t < prev_t:
+        var time = time_fmt % [cur_t / 60, cur_t % 60 % 60] 
+        prev_t = cur_t
+        if cur_t <= 10:
+            time = "[color=red]" + time
+            $"Beep".play()
+        $"TimeLabel".bbcode_text = time
