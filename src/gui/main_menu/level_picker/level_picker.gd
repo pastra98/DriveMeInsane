56b4827af02f1cf6c536c4c83195cebf0a5523c8
Paul Straberger <p.straberger@gmail.com>
2022-12-01 11:13:29 +0100

    there won't be enough levels to justify a grid container :(

--- src/gui/main_menu/level_picker/level_picker.gd
+++ src/gui/main_menu/level_picker/level_picker.gd
@@ -19,7 +19,7 @@ func _ready():
                 new_button.icon = load("res://gui/main_menu/level_picker/locked.png")
                 new_button.disabled = true
             new_button.connect("pressed", self, "level_button_pressed", [lvl_nr])
-            $"Panel/VBoxContainer/GridContainer".add_child(new_button)
+            $"Panel/VBoxContainer/VBoxContainer".add_child(new_button)
         file_name = dir.get_next()
 
 
